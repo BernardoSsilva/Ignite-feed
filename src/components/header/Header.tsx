c0f2c245d0f9d99a8ec8e1d95fe81d7a15bf7fc6
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-28 08:32:01 -0300

    created header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from "./header.module.css";
+import ignitelogo from "../../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.textContainer}>
+          <img src={ignitelogo} alt="logotipo do ignite" />
+          <strong>Ignite Feed</strong>
+        </div>
+      </header>
+    </>
+  );
+}
