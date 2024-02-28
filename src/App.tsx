20e08de19fb946a7af5528e538d3d7f132d96d22
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-28 09:00:10 -0300

    created sideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Header } from "./components/header/Header";
 import "./global-styles.css";
+import styles from "./app.module.css";
+import { SideBar } from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <body>
         <Header />
+
+        <div className={styles.wrapper}>
+          <SideBar />
+          <main></main>
+        </div>
       </body>
     </>
   );
